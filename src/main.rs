f7db23c543dbf7cee44038469e0ab002bdfc43f1
Martin Abeleda <martin.abeleda@gmail.com>
2022-04-14 21:32:04 -0700

    getting started

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+use rspotify::{
+    clients::{BaseClient, OAuthClient},
+    scopes, AuthCodeSpotify, Credentials, OAuth,
+};
+
 fn main() {
-    println!("Hello, world!");
+    // Initialise the client
+    let creds = Credentials::from_env().unwrap();
+    let oauth = OAuth::from_env(scopes!("user-read-currently-playing")).unwrap();
+    let mut spotify = AuthCodeSpotify::new(creds, oauth);
+
+    // Obtaining the access token
+    spotify.refetch_token();
+
+    let stream = spotify.current_user_saved_tracks(None);
+    println!("Items:");
+    for item in stream {
+        println!("* {}", item.unwrap().track.name);
+    }
+
+    // // Get a list of the users playlists
+    // let playlists = spotify.current_user_playlists();
+    // println!("Playlists:");
+    // for playlist in playlists {
+    //     println!("  * {}", playlist.unwrap().name);
+    // }
 }
